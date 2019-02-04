487363e8c98460e394f7564bc979d41ef09ae74d
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-02-04 06:28:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function takeANumber(katzDeli, name) {
   katzDeli.push(`${name}`);
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length) {
+    let firstPerson = katzDeliLine[0];
+  katzDeliLine.shift();
+  return ("Currently serving " + firstPerson + ".")
+} else {
+  return ( "There is nobody waiting to be served!")
+}
+}
