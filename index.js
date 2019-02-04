49044c971b367a688a4ff5906d8360e6897be35a
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-02-04 06:48:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ function nowServing(katzDeliLine) {
 let line = [];
 function currentLine(katzDeliLine) {
   for (let i=0; i<katzDeliLine.length; i++) {
-      line.push(" " + [i+1] + ". " + katzDeliLine[i])
+      line.push(" " + [i+1] + ". " + katzDeliLine[i]);
+    }
   if (katzDeliLine.length === 0) {
-    return ("The line is currently empty.");
+    return "The line is currently empty.";
   }else {
-    return ("The line is currently:" + line)
-  }
+    return "The line is currently:" + line;
+}
 }
