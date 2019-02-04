e24024e5c1f26411df3a2d7affca4219741f6466
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-02-04 06:41:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ function nowServing(katzDeliLine) {
   return ( "There is nobody waiting to be served!")
 }
 }
+let line = [];
+function currentLine(katzDeliLine) {
+  for (let i=0; i<katzDeliLine.length; i++) {
+      line.push(" " + [i+1] + "." + katzDeliLine[i] + ", ")
+  if (katzDeliLine.length === 0) {
+    return ("The line is currently empty.");
+  }else {
+    return ("The line is currently:" + line)
+  }
